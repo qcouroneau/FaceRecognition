b7e1436bc7b979fa54b4d69bce1db9bcd93e6ca3
Chad Benaaros <chad.benaaros33@gmail.com>
2022-05-18 14:55:01 +0200

    stash modifs

--- app/src/main/java/com/example/myapplication/Popup.java
+++ app/src/main/java/com/example/myapplication/Popup.java
@@ -1,46 +1,46 @@
-package com.example.myapplication;
-
-// https://www.youtube.com/watch?v=eBhgbhyyE_g
-
-import android.app.Activity;
-import android.app.Dialog;
-import android.widget.Button;
-import android.widget.TextView;
-
-public class Popup extends Dialog {
-
-    //fields
-    private String title;
-    private String subtitle;
-    private Button croixButton;
-    private TextView titleView, subTitleView;
-
-    //constructor
-    public Popup(Activity activity)
-    {
-        super(activity, R.style.Theme_AppCompat_DayNight_Dialog);
-        setContentView(R.layout.popup_template);
-        this.title = "Titre du popup";
-        this.subtitle = "Sous-titre";
-        this.croixButton = findViewById(R.id.croix_button);
-        this.titleView = findViewById(R.id.title_popup);
-        this.subTitleView = findViewById(R.id.subtitle_popup);
-
-
-    }
-
-    public void setTitle(String title) { this.title = title; }
-
-    public void setSubtitle(String subtitle) { this.subtitle = subtitle; }
-
-    public Button getRdvButton() { return croixButton; }
-
-    public void build(){
-        show();
-        titleView.setText(title);
-        subTitleView.setText(subtitle);
-
-    }
-
-
-}
+//package com.example.myapplication;
+//
+//// https://www.youtube.com/watch?v=eBhgbhyyE_g
+//
+//import android.app.Activity;
+//import android.app.Dialog;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//public class Popup extends Dialog {
+//
+//    //fields
+//    private String title;
+//    private String subtitle;
+//    private Button croixButton;
+//    private TextView titleView, subTitleView;
+//
+//    //constructor
+//    public Popup(Activity activity)
+//    {
+//        super(activity, R.style.Theme_AppCompat_DayNight_Dialog);
+//        setContentView(R.layout.popup_template);
+//        this.title = "Titre du popup";
+//        this.subtitle = "Sous-titre";
+//        this.croixButton = findViewById(R.id.croix_button);
+//        this.titleView = findViewById(R.id.title_popup);
+//        this.subTitleView = findViewById(R.id.subtitle_popup);
+//
+//
+//    }
+//
+//    public void setTitle(String title) { this.title = title; }
+//
+//    public void setSubtitle(String subtitle) { this.subtitle = subtitle; }
+//
+//    public Button getRdvButton() { return croixButton; }
+//
+//    public void build(){
+//        show();
+//        titleView.setText(title);
+//        subTitleView.setText(subtitle);
+//
+//    }
+//
+//
+//}
--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -35,14 +35,25 @@ import android.os.SystemClock;
 import android.util.Size;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import org.tensorflow.lite.examples.detection.databinding.ActivityMainBinding;
+
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.mlkit.vision.common.InputImage;
@@ -141,9 +152,30 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   //private HashMap<String, Classifier.Recognition> knownFaces = new HashMap<>();
 
 
+  private AppBarConfiguration appBarConfiguration;
+  private ActivityMainBinding binding;
+  private Toolbar toolbar;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    setSupportActionBar(binding.toolbar);
+
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+    binding.fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+      }
+    });
 
     fabAdd = findViewById(R.id.fab_add);
     fabAdd.setOnClickListener(new View.OnClickListener() {
@@ -167,7 +199,34 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
     faceDetector = detector;
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    return NavigationUI.navigateUp(navController, appBarConfiguration)
+            || super.onSupportNavigateUp();
+  }
 
   private void onAddClick() {
     addPending = true;
